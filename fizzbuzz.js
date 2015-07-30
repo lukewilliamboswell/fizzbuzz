c5996435debb31155ad5ef90dbb4de26a93ed208
Luke Boswell <lukewilliamboswell@gmail.com>
2015-07-30 10:47:57 +1000

    add js fizz buzz

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+// Print out fizz buzz from 1 to 100
+
+(function() {
+    for (var i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('Fizz Buzz')
+        } else if (i % 3 === 0) {
+            console.log('Fizz')
+        } else if (i % 5 === 0) {
+            console.log('Buzz')
+        } else {
+            console.log(i)
+        }
+    }
+})()
